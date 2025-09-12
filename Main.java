8a31c2f43d1cb5e6c5468aa98d2f84af2450bbf1
garzzaro <barbaragarzaro06@gmail.com>
2025-09-11 19:43:42 -0600

    commit o

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Connect4Controller.java juego = new Connect4Controller.java ();
+
+        juego.jugar(0, 0);
+        juego.jugar(1, 0);
+        juego.jugar(0, 1);
+        juego.jugar(1, 2);
+        juego.jugar(0, 2);
+    }
+}
